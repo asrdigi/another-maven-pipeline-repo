1a4e73543303c809d5d82b9e8f782e4e5d740a6c
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-06-22 16:54:27 +0530

    Minor changes

--- src/main/java/com/sapient/trg/pl/App.java
+++ src/main/java/com/sapient/trg/pl/App.java
@@ -4,6 +4,7 @@ package com.sapient.trg.pl;
 public class App {
     public static void main( String[] args )    {
         System.out.println( "Hello World!" );
+        System.out.println("Hello India");
         System.out.println("ThanQ");
     }
 }
