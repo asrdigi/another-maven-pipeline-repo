0511b8758484d649c796f27fefc8bd4e0a2850cc
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-06-22 16:49:57 +0530

    Updated the source file

--- src/main/java/com/sapient/trg/pl/App.java
+++ src/main/java/com/sapient/trg/pl/App.java
@@ -4,5 +4,6 @@ package com.sapient.trg.pl;
 public class App {
     public static void main( String[] args )    {
         System.out.println( "Hello World!" );
+        System.out.println("ThanQ");
     }
 }
